ac41dd011c40db77e0bcb234d0bae1c724a4bae5
Husnan Sultan <husnansultan@academytrainee.com>
2019-02-14 23:39:08 +0000

    a
    a

---
+++ work/clone.sh
@@ -0,0 +1,9 @@
+@echo off
+git clone https://github.com/alvinjo/AccountFront.git
+git clone https://github.com/alvinjo/RealAccountApi.git
+git clone https://github.com/alvinjo/RealAccountNumGenApi.git
+git clone https://github.com/alvinjo/RealAccountPrizeApi.git
+git clone https://github.com/alvinjo/RealAccountConsumer.git
+cd AccountFront
+npm install
+cd ..
---
+++ work/starter.sh
@@ -0,0 +1,9 @@
+ttab 'cd RealAccountApi; mvn spring-boot:run'
+
+ttab 'cd RealAccountConsumer; mvn spring-boot:run'
+
+ttab 'cd RealAccountPrizeApi; mvn spring-boot:run'
+
+ttab 'cd RealAccountNumGenApi; mvn spring-boot:run'
+
+ttab 'cd AccountFront; npm start'
